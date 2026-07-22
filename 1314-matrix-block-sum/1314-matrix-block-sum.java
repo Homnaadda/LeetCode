59cda2537381cc64e79966b869f9f490c9505d3d
M.Homnaadda <chanti20017@gmail.com>
2026-07-22 08:31:37 +0530

    Time: 7 ms (67.67%), Space: 46.6 MB (60.98%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.java
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -2,20 +2,29 @@ class Solution {
     public int[][] matrixBlockSum(int[][] mat, int k) {
         int m = mat.length;
         int n = mat[0].length;
-        int[][] answer = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                int sum = 0;
-                for(int r = i-k;r<=i+k;r++){
-                    for(int c = j-k;c<=j+k;c++){
-                        if(r>=0 && r<m && c>=0 && c<n){
-                            sum+=mat[r][c];
-                        }
-                    }
-                }
-                answer[i][j] = sum;
+
+        int [][] pref = new int[m+1][n+1];
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                pref[i][j] = mat[i-1][j-1] +pref[i-1][j] +pref[i][j-1]- pref[i-1][j-1];
             }
         }
-        return answer;
+        int[][] ans = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int r1 = Math.max(0,i-k);
+                int c1 = Math.max(0,j-k);
+                int r2 = Math.min(m-1,i+k);
+                int c2 = Math.min(n-1,j+k);
+
+                r1++;
+                c1++;
+                r2++;
+                c2++;
+
+                ans[i][j] = pref[r2][c2]-pref[r1-1][c2]-pref[r2][c1-1]+pref[r1-1][c1-1];
+            }
+        }
+        return ans;
     }
 }
